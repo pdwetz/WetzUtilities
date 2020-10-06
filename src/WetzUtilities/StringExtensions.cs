27d39ae6b58637e649b15b314563e46c9c6e27f4
Peter Wetzel <pdwetz@gmail.com>
2020-10-05 20:28:59 -0400

    Moderate version bump for Interface change: GetNextName now allows overriding the separator character (defaults to '-', which is unchanged). Added missing license header for some source files as well as some other housecleaning.

--- src/WetzUtilities/StringExtensions.cs
+++ src/WetzUtilities/StringExtensions.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2019 Peter Wetzel
+Copyright 2020 Peter Wetzel
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -154,7 +154,6 @@ namespace WetzUtilities
                     return TimeSpan.ParseExact(source, @"m\:ss", null);
                 default:
                     return TimeSpan.ParseExact(source, @"ss", null);
-
             }
         }
 
